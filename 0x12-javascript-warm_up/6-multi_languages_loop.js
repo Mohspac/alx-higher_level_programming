9e4d2d152479b26f6d99d47fe48427bf1f8a520b
Olawale Fatokun <ola.wale9@gmail.com>
2023-03-14 04:53:17 -0700

    javascriptWarmup

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const language of languages) {
+  console.log(language);
 }
